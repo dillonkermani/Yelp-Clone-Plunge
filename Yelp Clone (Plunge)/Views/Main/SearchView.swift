b9968552e1412d89c3da678f601d7451cc4fef3d
dillonkermani <60326614+dillonkermani@users.noreply.github.com>
2023-08-09 22:45:21 -0700

    Added SearchBar + Dismiss Functionality

--- Yelp Clone (Plunge)/Views/Main/SearchView.swift
+++ Yelp Clone (Plunge)/Views/Main/SearchView.swift
@@ -11,32 +11,28 @@ struct SearchView: View {
     var body: some View {
         
         let images = ["vector1", "vector2", "vector3", "vector4"]
+        
+        @State var searchText = ""
                 
-        VStack {
-            
-            GeometryReader { geometry in
-                ImageCarouselView(numberOfImages: 4) {
-                    ForEach(0..<4) { i in
-                        Image("\(images[i])")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                            .clipped()
-                    }
-                }.clipped()
-            }.frame(width: SCREEN_WIDTH, height: SCREEN_HEIGHT/3.5)
-            
+        ZStack {
             ScrollView {
                 VStack {
-                    Rectangle()
-                        .foregroundColor(.gray)
-                        .frame(width: SCREEN_WIDTH, height: SCREEN_HEIGHT)
+                    GeometryReader { geometry in
+                        ImageCarouselView(numberOfImages: 4) {
+                            ForEach(0..<4) { i in
+                                Image("\(images[i])")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: geometry.size.width, height: geometry.size.height)
+                                    .clipped()
+                            }
+                        }.clipped()
+                    }.frame(width: SCREEN_WIDTH, height: SCREEN_HEIGHT/3.5)
+                    
+                    SearchBar(text: searchText)
+                    
                 }
             }
-            
-            
-            
-            Spacer()
         }
     }
 }
