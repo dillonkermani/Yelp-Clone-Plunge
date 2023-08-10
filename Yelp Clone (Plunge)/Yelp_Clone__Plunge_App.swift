236509d3a006bd3893adbfd7543a68002c49821d
dillonkermani <60326614+dillonkermani@users.noreply.github.com>
2023-08-09 19:37:39 -0700

    Initial Commit

---
+++ Yelp Clone (Plunge)/Yelp_Clone__Plunge_App.swift
@@ -0,0 +1,17 @@
+//
+//  Yelp_Clone__Plunge_App.swift
+//  Yelp Clone (Plunge)
+//
+//  Created by Dillon Kermani on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Yelp_Clone__Plunge_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
